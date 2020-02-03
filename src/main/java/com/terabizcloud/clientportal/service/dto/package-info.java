0e1b1587b892c1f588ed4b22f3fd5a9e10f79a49
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-03 11:23:44 +0300

    Remove old projects

--- src/main/java/com/terabizcloud/clientportal/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.terabizcloud.clientportal.service.dto;
--- src/main/java/com/terabizcloud/clientportal/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package com.terabizcloud.clientportal.web.rest;
